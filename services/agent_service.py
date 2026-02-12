afa46eb848318c7b531db1b8e9a27cd8d19668cf
Sweet~Zzx <3082269605@qq.com>
2026-02-12 12:31:06 +0800

    升级模型到 GLM-5

--- services/agent_service.py
+++ services/agent_service.py
@@ -48,7 +48,7 @@ class ScheduleAgentService:
 - 不要主动推销日程功能
 - 请用中文回复"""
 
-    def __init__(self, zhipu_api_key: str, model: str = "glm-4"):
+    def __init__(self, zhipu_api_key: str, model: str = "glm-5"):
         """
         初始化 Agent 服务
 
